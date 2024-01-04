67236262e8f8d967ad59dabcdbcb6b1a5d68a0d5
Jimmy <1406218553@qq.com>
2024-01-04 22:07:55 +0800

    multi-thread

--- Software/screen.py
+++ Software/screen.py
@@ -1,19 +1,47 @@
-import sys
 import os
+import sys
 import subprocess
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QSplitter, 
-    QSpacerItem, QSizePolicy, QMessageBox)
+    QApplication, QWidget, QVBoxLayout, QHBoxLayout, 
+    QPushButton, QFileDialog, QMessageBox, QSpacerItem,
+    QSizePolicy, QMainWindow
+)
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 from model import ModelViewer
 from camera import CameraApp
 
-class IntegratedApp(QWidget):
-    # 多线程  
+# 定义一个新的线程
+class ReconstructionThread(QThread):
+    finished = pyqtSignal(str, str, str)            # 信号，用于在操作完成时发出通知
 
+    def __init__(self, input_path, output_folder, file_name):
+        super().__init__()
+        self.input_path = input_path
+        self.output_folder = output_folder
+        self.file_name = file_name
+
+    def run(self):
+        try:
+            # 执行三维重建代码
+            demo_script_path = os.path.normpath("C:\\Users\\Administrator\\Desktop\\DECA_Analyze\\demos\\demo_reconstruct.py")
+            command = f"python {demo_script_path} -i {self.input_path} -s {self.output_folder} --saveObj True"
+            subprocess.run(command, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            # 发送错误信息
+            self.finished.emit(f"三维重建失败，错误信息: {e}", "", "")
+            return
+
+        # 通知主线程操作完成
+        self.finished.emit("", self.output_folder, self.file_name)
+        
+
+# 主界面类
+class IntegratedApp(QWidget):
     def __init__(self):
         super().__init__()
+        # 添加重建成员变量
+        self.reconstruction_thread = None
 
         # 设置窗口标题和大小
         self.setWindowTitle("Picture to Model")
@@ -82,23 +110,23 @@ class IntegratedApp(QWidget):
         file_name = os.path.splitext(os.path.basename(input_path))[0]
         output_folder = os.path.join(image_directory, file_name)
 
-        self.model_viewer.cleanModel()                                               # 清除现有模型
-        
-        # demo_reconstruct.py的位置
-        demo_script_path = os.path.normpath("C:\\Users\\Administrator\\Desktop\\DECA_Analyze\\demos\\demo_reconstruct.py")
+        self.model_viewer.cleanModel()  # 清除现有模型
 
-        # 构建命令行命令
-        command = f"python {demo_script_path} -i {input_path} -s {output_folder} --saveObj True"
+        # 创建重建线程
+        self.reconstruction_thread = ReconstructionThread(input_path, output_folder, file_name)
+        self.reconstruction_thread.finished.connect(self.reconstruction_finished)  # 连接信号
 
-        # 运行命令
-        try:
-            subprocess.run(command, shell=True, check=True)
-            obj_path = os.path.join(output_folder, file_name, file_name + '.obj')    # 糙模型路径
-            self.model_viewer.loadModel(obj_path)                                    # 加载模型
+        # 启动线程
+        self.reconstruction_thread.start()
+
+    def reconstruction_finished(self, error_message, output_folder, file_name):
+        if error_message:
+            QMessageBox.critical(self, "错误", error_message)
+        else:
+            # 三维重建完成，执行界面更新
+            obj_path = os.path.join(output_folder, file_name, file_name + '.obj')  # 糙模型路径
+            self.model_viewer.loadModel(obj_path)  # 加载模型
             QMessageBox.information(self, "成功", "三维重建完成!")
-        except subprocess.CalledProcessError as e:
-            QMessageBox.critical(self, "错误", f"三维重建失败，错误信息: {e}")
-            
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
